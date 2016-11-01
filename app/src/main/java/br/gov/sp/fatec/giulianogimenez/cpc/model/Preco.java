99bcca4764531565131de1b27a50e41541541a9d
Giuliano Gimenez <ggimenez87@gmail.com>
2016-11-01 20:48:57 -0200

    Início do cadastro dos preços.

--- app/src/main/java/br/gov/sp/fatec/giulianogimenez/cpc/model/Preco.java
+++ app/src/main/java/br/gov/sp/fatec/giulianogimenez/cpc/model/Preco.java
@@ -10,13 +10,12 @@ import br.gov.sp.fatec.giulianogimenez.cpc.DAO.DatabaseInfo;
  * Created by giuliano.gimenez on 30/09/2016.
  */
 public class Preco extends SQLiteOpenHelper {
+
     private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + PrecoInfo.TABLE_NAME + " (" +
-            PrecoInfo.PRC_ID + DatabaseInfo.INT_TYPE + " PRIMARY KEY," +
+            PrecoInfo.PRC_ID + DatabaseInfo.INT_TYPE + " PRIMARYKEY " + DatabaseInfo.COMMA_SEP +
             PrecoInfo.PRC_VALOR + DatabaseInfo.DECIMAL_TYPE + DatabaseInfo.COMMA_SEP +
-            PrecoInfo.PRC_DATACADASTRO + DatabaseInfo.DATETIME_TYPE + DatabaseInfo.COMMA_SEP +
             PrecoInfo.PRC_TIPOCOMBUSTIVEL + DatabaseInfo.TEXT_TYPE + DatabaseInfo.COMMA_SEP +
-            PrecoInfo.EST_ID + DatabaseInfo.INT_TYPE + DatabaseInfo.COMMA_SEP +
-            "FOREIGN KEY(" + PrecoInfo.EST_ID + ") REFERENCES " +  Estabelecimento.EstabelecimentoInfo.TABLE_NAME + "(" + Estabelecimento.EstabelecimentoInfo.EST_ID + "));";
+            PrecoInfo.EST_ID + DatabaseInfo.INT_TYPE + ");";
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + PrecoInfo.TABLE_NAME;
 
